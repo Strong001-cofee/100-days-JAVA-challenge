31712b54bf484a8026366ee7e8860103c1c5c25c
Strong Coffee <sahooanish436@gmail.com>
2025-03-05 23:40:04 +0530

    100 days java challenge

---
+++ area_of_rect_d1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class area_of_rect_d1 {
+    public static void main(String[] args) {
+        float a,b,c;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextFloat();
+        b = sc.nextFloat();
+        c=a*b;
+        System.out.println(c);
+    }
+}
---
+++ sum_of_two_numbers_d2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sum_of_two_numbers_d2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println(c);
+    }
+}
